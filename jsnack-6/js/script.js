693ac4c8511fa04f14824b04380460b4cee8f802
Martina Amore <martina.amore02@gmail.com>
2020-11-12 18:31:04 +0100

    Completi

---
+++ jsnack-6/js/script.js
@@ -0,0 +1,31 @@
+// Jsnack 6
+// dato questo array di obj iniziale:
+// var arrayObj = [
+//   {name: 'Poppy', type: 'tshirt', color: 'red'},
+//   {name: 'Jumping', type: 'occhiali', color: 'blue'},
+//   {name: 'CrissCross', type: 'scarpe', color: 'black'},
+//   {name: 'Jenny', type: 'borsa', color: 'pink'},
+// ];
+// Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array)
+// una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale
+
+const arrayObj = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
+
+const copiaArrayObj = [...arrayObj];
+
+copiaArrayObj.forEach((element) => {
+    element["position"] = letterRandom();
+})
+
+function letterRandom() {
+    var letter = "abcdefghilmnopqrstuvz";
+    return letter.charAt(Math.floor(Math.random()* letter.length));
+}
+
+console.log(arrayObj);
+console.log(copiaArrayObj);
